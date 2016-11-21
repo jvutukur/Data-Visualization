d08f45cd8bdc16d0b1557cff236359f4d8519818
jvutukur <jvutukur@asu.edu>
2016-11-21 00:11:07 -0700

    added time line

---
+++ Chropleth/jScripts/timeLineCreation.js
@@ -0,0 +1,70 @@
+function createTimeLine(){
+// set the dimensions and margins of the graph
+
+
+var margin = {top: 20, right: 20, bottom: 30, left: 50},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+// set the ranges
+var x = d3.scaleTime().range([0, width]);
+var y = d3.scaleLinear().range([height, 0]);
+
+// define the area
+var area = d3.area()
+    .x(function(d) { return x(d.date); })
+    .y0(height)
+    .y1(function(d) { return y(d.close); });
+
+// define the line
+var valueline = d3.line()
+    .x(function(d) { return x(d.date); })
+    .y(function(d) { return y(d.close); });
+
+// append the svg obgect to the body of the page
+// appends a 'group' element to 'svg'
+// moves the 'group' element to the top left margin
+$("#TimeLineContent").html('');
+var svg = d3.select("#TimeLineContent").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
+
+// get the data
+d3.csv("data_timeLine_Bhaddy.csv", function(error, data) {
+  if (error) throw error;
+
+  // format the data
+  data.forEach(function(d) {
+      d.date = new Date(d.date);
+      d.close = +d.close;
+  });
+
+  // scale the range of the data
+  x.domain(d3.extent(data, function(d) { return d.date; }));
+  y.domain([d3.min(data,function(d){return d.close}), d3.max(data, function(d) { return d.close; })]);
+  // add the area
+    svg.append("path")
+       .data([data])
+       .attr("class", "area")
+       .attr("d", area);
+
+  // add the valueline path.
+  svg.append("path")
+      .data([data])
+      .attr("class", "line")
+      .attr("d", valueline);
+
+  // add the X Axis
+  svg.append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x));
+
+  // add the Y Axis
+  svg.append("g")
+      .call(d3.axisLeft(y));
+
+});
+}
